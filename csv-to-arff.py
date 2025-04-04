644d2688f0c28a621ee6d326618b88be5be11811
Interlated Interlated <contact@interlated.com.au>
2025-04-04 11:00:59 +1100

    updated quoting

--- csv-to-arff.py
+++ csv-to-arff.py
@@ -1,4 +1,4 @@
-# Converts comma separated value (CSV) files 
+# Converts comma separated value (CSV) files
 # to Attribute-Relation File Format (ARFF).
 # where 'converts' means that an arff file is created and populated with
 # the csv data. The csv file is not deleted or modified.
@@ -9,15 +9,15 @@
 # Python 3.6
 #
 # @Description
-# Simple python program that reads a csv file, selects all its attributes 
+# Simple python program that reads a csv file, selects all its attributes
 # and assigns its data type ("numeric" or "nominal").
-# Selects unique data values for each nominal attribute, and 
+# Selects unique data values for each nominal attribute, and
 # inserts a '0' on each empty cell.
 #
 # This program was made to facilitate some csv data cleaning when I was
 # trying to open a csv file in Weka, for a school research project.
 # This program helps to clean the csv file by converting it to arff format
-# when the csv file has some inconsistencies, such as having numeric and 
+# when the csv file has some inconsistencies, such as having numeric and
 # nominal values for the same attribute values, and when it has empty cells.
 #
 # @About the ARFF format and Weka Software
@@ -33,104 +33,111 @@
 import csv
 import os
 
-fileToRead = "worldcup2014.csv"  #csv file name or absolute path to be open.
-fileToWrite = "worldcup2014.arff" #name as how you'll save your arff file.
-relation = "World Cup 2014" #how you'll like to call your relation as.
 
-dataType = [] # Stores data types 'nominal' and 'numeric'
-columnsTemp = [] # Temporary stores each column of csv file except the attributes
-uniqueTemp = [] # Temporary Stores each data cell unique of each column
-uniqueOfColumn = [] # Stores each data cell unique of each column
-dataTypeTemp = [] # Temporary stores the data type for cells on each column
-finalDataType = [] # Finally stores data types 'nominal' and 'numeric'
-attTypes = [] # Stores data type 'numeric' and nominal data for attributes
-p = 0 # pointer for each cell of csv file
+def csv_to_arff(fileToRead, fileToWrite, relation):
+    """
+    Converts a CSV file to ARFF format, handling quoted fields, empty cells in data.
 
-writeFile = open(fileToWrite, 'w')
+    Args:
+        fileToRead (str): The name or absolute path of the CSV file to read.
+        fileToWrite (str): The name of the ARFF file to create.
+        relation (str): The relation name for the ARFF file.
+    """
 
-#Opening and Reading a CSV file
-f = open(fileToRead, 'r')
-reader = csv.reader(f)
-allData = list(reader)
-attributes = allData[0]
-totalCols = len(attributes)
-totalRows = len(allData)
-f.close()
+    dataType = []
+    columnsTemp = []
+    uniqueTemp = []
+    uniqueOfColumn = []
+    dataTypeTemp = []
+    finalDataType = []
+    attTypes = []
+    p = 0
 
-# Add a '0' for each empty cell
-for j in range(0,totalCols):
-	for i in range(0,totalRows):
-		if 0 == len(allData[i][j]):
-			allData[i][j] = "0"
+    try:
+        with open(fileToRead, 'r', newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            allData = list(reader)
+    except FileNotFoundError:
+        print(f"Error: File '{fileToRead}' not found.")
+        return
+    except Exception as e:
+        print(f"Error reading file '{fileToRead}': {e}")
+        return
 
-# check for comams or blanks and adds single quotes
-for j in range(0,totalCols):
-	for i in range(1,totalRows):
-		allData[i][j] = allData[i][j].lower()
-		if "\r" in allData[i][j] or '\r' in allData[i][j] or "\n" in allData[i][j] or '\n' in allData[i][j]:
-			allData[i][j] = allData[i][j].rstrip(os.linesep)
-			allData[i][j] = allData[i][j].rstrip("\n")
-			allData[i][j] = allData[i][j].rstrip("\r")
-		try:
-			if allData[i][j] == str(float(allData[i][j])) or allData[i][j] == str(int(allData[i][j])):
-				print
-		except ValueError as e:
-				allData[i][j] = "'" + allData[i][j] + "'"
+    attributes = allData[0]
+    totalCols = len(attributes)
+    totalRows = len(allData)
 
-# fin gives unique cells for nominal and numeric
-for j in range(0,totalCols):
-	for i in range(1,totalRows):
-		columnsTemp.append(allData[i][j])
-	for item in columnsTemp:
-		if not (item in uniqueTemp):
-			uniqueTemp.append(item)
-	uniqueOfColumn.append("{" + ','.join(uniqueTemp) + "}") 
-	uniqueTemp = []
-	columnsTemp = []
+    # Add '0' for empty cells
+    for j in range(0, totalCols):
+        for i in range(0, totalRows):
+            if not allData[i][j]:
+                allData[i][j] = "0"
 
-# Assigns numeric or nominal to each cell
-for j in range(1,totalRows):
-	for i in range(0,totalCols):
-		try:
-			if allData[j][i] == str(float(allData[j][i])) or allData[j][i] == str(int(allData[j][i])):
-				dataType.append("numeric")
-		except ValueError as e:
-				dataType.append("nominal")
+    # Handle commas, blanks, and quotes
+    for j in range(0, totalCols):
+        for i in range(1, totalRows):
+            allData[i][j] = allData[i][j].lower()
+            allData[i][j] = allData[i][j].strip(os.linesep).strip("\n").strip("\r").replace('\'', r'\'').replace('\"',
+                                                                                                                 r'\"')
+            try:
+                float(allData[i][j])
+            except ValueError:
+                allData[i][j] = '"' + allData[i][j] + '"'
 
-for j in range(0,totalCols): 
-	p = j
-	for i in range(0,(totalRows-1)): 
-		dataTypeTemp.append(dataType[p])
-		p += totalCols  
-	if "nominal" in dataTypeTemp:
-		finalDataType .append("nominal")
-	else:
-		finalDataType .append("numeric")
-	dataTypeTemp = []
+    # Find unique cells for nominal and numeric
+    for j in range(0, totalCols):
+        for i in range(1, totalRows):
+            columnsTemp.append(allData[i][j])
+        uniqueTemp = list(dict.fromkeys(columnsTemp))
+        uniqueOfColumn.append("{" + ','.join(uniqueTemp) + "}")
+        columnsTemp = []
 
-for i in range(0,len(finalDataType )):
-	if finalDataType [i] == "nominal":
-		attTypes.append(uniqueOfColumn[i])
-	else:
-		attTypes.append(finalDataType[i])
+    # Assign numeric or nominal to each cell
+    for j in range(1, totalRows):
+        for i in range(0, totalCols):
+            try:
+                float(allData[j][i])
+                dataType.append("numeric")
+            except ValueError:
+                dataType.append("nominal")
 
-# Show comments
-writeFile.write("%\n% Comments go after a '%' sign.\n%\n")
-writeFile.write("%\n% Relation: " + relation +"\n%\n%\n")
-writeFile.write("% Attributes: " + str(totalCols) + " "*5 
-	+ "Instances: " + str(totalRows-1) + "\n%\n%\n\n")
+    for j in range(0, totalCols):
+        p = j
+        for i in range(0, (totalRows - 1)):
+            dataTypeTemp.append(dataType[p])
+            p += totalCols
+        if "nominal" in dataTypeTemp:
+            finalDataType.append("nominal")
+        else:
+            finalDataType.append("numeric")
+        dataTypeTemp = []
 
-# Show Relation
-writeFile.write("@relation " + relation + "\n\n")
+    for i in range(0, len(finalDataType)):
+        if finalDataType[i] == "nominal":
+            attTypes.append(uniqueOfColumn[i])
+        else:
+            attTypes.append(finalDataType[i])
 
-# Show Attributes
-for i in range(0,totalCols):
-	writeFile.write("@attribute" + " '" + attributes[i] 
-		+ "' " + attTypes[i] + "\n")
+    # Write to ARFF file
+    try:
+        with open(fileToWrite, 'w', encoding='utf-8') as writeFile:
+            writeFile.write("%\n% Comments go after a '%' sign.\n%\n")
+            writeFile.write("%\n% Relation: " + relation + "\n%\n%\n")
+            writeFile.write(
+                "% Attributes: " + str(totalCols) + " " * 5 + "Instances: " + str(totalRows - 1) + "\n%\n%\n\n")
+            writeFile.write("@relation \"" + relation + "\"\n\n")
+            for i in range(0, totalCols):
+                writeFile.write("@attribute" + " '" + attributes[i] + "' " + attTypes[i] + "\n")
+            writeFile.write("\n@data\n")
+            for i in range(1, totalRows):
+                writeFile.write(','.join(allData[i]) + "\n")
 
-# Show Data
-writeFile.write("\n@data\n")
-for i in range(1,totalRows):
-	writeFile.write(','.join(allData[i])+"\n")
+        print(f"'{fileToWrite}' was converted from '{fileToRead}'")
 
-print(fileToWrite + " was converted to " + fileToRead)
+    except Exception as e:
+        print(f"Error writing to file '{fileToWrite}': {e}")
+
+
+# Example usage:
+csv_to_arff("gb-data-20221124.csv", "gb-data20221124.arff", "Green Bonds")
